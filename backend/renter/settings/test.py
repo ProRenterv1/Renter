c2c6cd93c24b2cfe50fa3371a1cddd8205e6aecc
Oleksandr Lisnytskyi <oleksandrl1009@gmail.com>
2025-11-09 23:50:28 -0700

    chore(tests): fix tests config

--- backend/renter/settings/test.py
+++ backend/renter/settings/test.py
@@ -1,8 +1,12 @@
+import os
+
 from .base import *
 
 DEBUG = True
 USE_S3 = False  # ensure local storage in tests
-
+CELERY_TASK_ALWAYS_EAGER=True 
+CELERY_TASK_EAGER_PROPAGATES=True
+STORAGE_SKIP_TASK_EXECUTION=True
 SECRET_KEY = os.environ.get("SECRET_KEY", "test-secret-key")
 
 # SQLite for CI speed/simplicity if DATABASE_URL absent
